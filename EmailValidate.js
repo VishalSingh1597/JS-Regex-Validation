00f097004783c9db9c66df94714d097d86380346
Vishal Singh <VishalSingh1597>
2021-06-05 03:08:54 +0530

    [Vishal]Added:Program to validate @ symbol added

--- EmailValidate.js
+++ EmailValidate.js
@@ -1,9 +1,10 @@
+//using prompt-sync module function to take input
 const readline = require("prompt-sync");
 const prompt=readline();
 //taking user input of email id which we need to validate
 let email = prompt("Enter Email : ");
-//regex to validate email before @
-const emailRegex = RegExp('^([A-Z a-z 0-9]+)[A-Z a-z 0-9 !@#$%&*.]*$');
+//regex to validate email before @ and ensure @ symbol
+const emailRegex = RegExp('^[A-Z a-z 0-9 !#$%&*.]{1,}[@]{1}[A-Z a-z 0-9 !#$%&*.]*$');
 //with test method checking email valid or not
 if(emailRegex.test(email))
 	console.log("Valid Pincode!");
