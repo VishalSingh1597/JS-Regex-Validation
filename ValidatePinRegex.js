e8b8919f9fd160ce9019f807f51391fe52251e22
Vishal Singh <VishalSingh1597>
2021-06-05 02:35:03 +0530

    [Vishal]Added:Program to validate 6 digit pin with RegExp added

---
+++ ValidatePinRegex.js
@@ -0,0 +1,12 @@
+//using prompt-sync module function to take input
+const readline = require("prompt-sync");
+const prompt=readline();
+//taking user input
+let pin = prompt("Enter PIN : ");
+//regular expresion for 6 digit code
+let pinRegExp = RegExp('^[1-9]{1}[0-9]{5}$');
+//with test method checking if pin is valid or not and print
+if(pinRegExp.test(pin))
+	console.log("Valid Pincode");
+else
+	console.log("Invalid Pincode");
